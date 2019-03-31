88850c75b7d3ab8be4bffe25b6edbb75e610a967
AleRoman <39933694+AleRoman@users.noreply.github.com>
2019-03-31 01:43:59 -0400

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyDTXxwXvgIbG7a2U0-8grUQU90NLj81tAc',
+    authDomain: 'findme-23ded.firebaseapp.com',
+    databaseURL: 'https://findme-23ded.firebaseio.com',
+    projectId: 'findme-23ded',
+    storageBucket: 'findme-23ded.appspot.com',
+    messagingSenderId: '467427568426'
+  }
 };
 
 /*
